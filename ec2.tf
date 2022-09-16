05d836827e4d1aae4d59b9c377c88eb6e378ce06
surya <suryapatneedi0505@gmai.ocm>
2022-09-16 11:01:05 +0530

    adding security groups

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
-  vpc_security_group_ids = "sg-09f666e4e00dff33e"
+  security_group_ids = "sg-09f666e4e00dff33e"
 
   tags = {
     Name = "HelloWorld"
