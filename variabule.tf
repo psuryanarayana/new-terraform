4630a54d0870635032d2d07f6368c7910db9d71a
Kasi k <surya.oracle8184@gmail.com>
2023-08-29 13:16:19 +0530

    changes

--- variabule.tf
+++ variabule.tf
@@ -2,13 +2,13 @@ variable "ami"{
     default= "ami-0bb6af715826253bf"
 }
 
-variable "instance_type" {
+variable "instance_" {
     default = "t3.micro"
   
 }
 
 
-variable "security_group_ids" {
+variable "security_group" {
     default = "sg-09f666e4e00dff33e"
   
 }
